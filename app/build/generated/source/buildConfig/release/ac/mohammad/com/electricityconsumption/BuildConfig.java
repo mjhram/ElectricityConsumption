0476cd305396613a5d7eb4fc657909b49a3c2193
hp <mjhram20002>
2022-12-29 23:09:14 +0300

    Adding GraphView

--- app/build/generated/source/buildConfig/release/ac/mohammad/com/electricityconsumption/BuildConfig.java
+++ app/build/generated/source/buildConfig/release/ac/mohammad/com/electricityconsumption/BuildConfig.java
@@ -7,6 +7,6 @@ public final class BuildConfig {
   public static final boolean DEBUG = false;
   public static final String APPLICATION_ID = "ac.mohammad.com.electricityconsumption";
   public static final String BUILD_TYPE = "release";
-  public static final int VERSION_CODE = 27;
-  public static final String VERSION_NAME = "2.7";
+  public static final int VERSION_CODE = 28;
+  public static final String VERSION_NAME = "2.8";
 }
